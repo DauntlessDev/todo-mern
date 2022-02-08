a2e6a0e63e037eec17c077531ef2dce74a8ad6aa
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-02-08 10:01:03 +0800

    done setting up the mongodb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+
+
+mongoose.connect("mongodb+srv://user123:Password123Brave@cluster0.vln9r.mongodb.net/TodoDb?retryWrites=true&w=majority")
 
 app.listen(3001, () => {
   console.log("Listening to the server...");
 });
+
