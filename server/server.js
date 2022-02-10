9b9ac16c2e2733e4fa4c12e89a30792ddc4d3e35
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-02-10 16:18:12 +0800

    working get and add todo for backend

--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const TodoModel = require("./models/Todos");
 
 const cors = require("cors");
 
+app.use(express.json());
+app.use(cors());
 
-mongoose.connect("mongodb+srv://user123:Password123Brave@cluster0.vln9r.mongodb.net/TodoDb?retryWrites=true&w=majority")
+mongoose.connect(
+  "mongodb+srv://user123:Password123Brave@cluster0.vln9r.mongodb.net/TodoDb?retryWrites=true&w=majority"
+);
+
+app.get("/", (req, res) => {
+  TodoModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/addTodo", async (req, res) => {
+  const todo = req.body;
+  const newTodo = new TodoModel(todo);
+  await newTodo.save();
+
+  res.json(todo);
+});
 
 app.listen(3001, () => {
   console.log("Listening to the server...");
 });
-
