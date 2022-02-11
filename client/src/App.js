c085b89530ee825fe4b989254ec54e2323e47671
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-02-11 10:17:29 +0800

    added get todolist to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,29 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [title, setTitle] = useState("todo title");
+  const [state, setCompleted] = useState(false);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/").then((response) => {
+      setTodoList(response.data);
+    });
+  });
+
   return (
-    <div className="App">test
+    <div className="App">
+      <div className="main">
+        {todoList.map((todo) => {
+          return todo.state ? (
+            <h1 className="completed">Title: {todo.title}</h1>
+          ) : (
+            <h1 className="notcompleted">Title: {todo.title}</h1>
+          );
+        })}
+      </div>
     </div>
   );
 }
