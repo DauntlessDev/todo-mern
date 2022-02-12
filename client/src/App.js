b3bd40c3aa8bac84175f087a654d9e9faa878cff
DauntlessDev <45929622+DauntlessDev@users.noreply.github.com>
2022-02-12 15:23:50 +0800

    added addtodo in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Axios from "axios";
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [title, setTitle] = useState("todo title");
-  const [state, setCompleted] = useState(false);
+  // const [state, setCompleted] = useState(false);
 
   useEffect(() => {
     Axios.get("http://localhost:3001/").then((response) => {
@@ -13,16 +13,40 @@ function App() {
     });
   });
 
+  const addTodo = () => {
+    Axios.post("http://localhost:3001/addTodo", {
+      title,
+      state: false,
+    }).then((response) => {
+      setTodoList([...todoList], { title, state: false });
+    });
+  };
+
+  const toggleState = () => {};
+
   return (
     <div className="App">
       <div className="main">
         {todoList.map((todo) => {
-          return todo.state ? (
-            <h1 className="completed">Title: {todo.title}</h1>
-          ) : (
-            <h1 className="notcompleted">Title: {todo.title}</h1>
+          return (
+            <button onClick={toggleState}>
+              <h1 className={todo.state ? "completed" : "notcompleted"}>
+                Title: {todo.title}
+              </h1>
+            </button>
           );
         })}
+
+        <div>
+          <input
+            type="tyext"
+            placeholder="add todo..."
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
+          <button onClick={addTodo}>ADD</button>
+        </div>
       </div>
     </div>
   );
